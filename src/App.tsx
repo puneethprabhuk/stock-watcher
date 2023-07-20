495a7ef607ebfd27173f44224cd34a226a0713cf
puneethprabhu <puneethprabhuk@gmail.com>
2023-07-20 12:38:22 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  console.log(process.env.REACT_APP_API_HOST);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <React.Fragment>
 
+    </React.Fragment>
+  )
+}
 export default App;
